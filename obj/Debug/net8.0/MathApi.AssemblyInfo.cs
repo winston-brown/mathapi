eb33c113746e03e94dd054a834095d00c08ca7ca
Winston Brown <winston@winstonbrown.me>
2024-10-13 20:53:46 -0500

    edit readme.md

--- obj/Debug/net8.0/MathApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MathApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MathApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cc1d9e760f60b725b0783534c317dd6eed82ae5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3f7f43f1965e45aa3d8b5e6f2306b8275212abf")]
 [assembly: System.Reflection.AssemblyProductAttribute("MathApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MathApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
